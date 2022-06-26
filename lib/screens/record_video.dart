ffe15e43b5e008c8356ab8382d0b90a8c9c3b556
philippe-heitzmann <phil062497@gmail.com>
2022-06-26 14:50:39 -0400

    init commit

---
+++ lib/screens/record_video.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'package:receivesharing/ui/home/home_screen.dart';
+import 'package:receivesharing/ui/home/home_panel.dart';
+import 'package:receivesharing/ui/pages/camera_screen.dart';
+
+class RecordVideo extends StatefulWidget {
+  @override
+  _RecordVideoState createState() => _RecordVideoState();
+}
+
+class _RecordVideoState extends State<RecordVideo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: Container(
+                    width: 200,
+                    height: 150,
+                    /*decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(50.0)),*/
+                ),
+              ),
+            ),
+            Container(
+              height: 50,
+              width: 350,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => CameraScreen()));
+                },
+                child: Text(
+                  'Start Recording New Video',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 130,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
